fb81c576d3794b1db3f6502b739bc56e4a79b3e7
kingofstrong697 <giahungcv2012@gmail.com>
2025-03-31 05:10:36 +0700

    script.js

--- script.js
+++ script.js
@@ -384,15 +384,3 @@ document.addEventListener('DOMContentLoaded', () => {
     viewAccountsButton.addEventListener('click', () => {
         showAccountsModal();
     });
-
-   
-
-    logoutButton.addEventListener('click', () => {
-        hideUserInfo();
-        alert('Đăng xuất thành công!');
-        giftMoneyButton.style.display = 'none';
-        messagesButton.style.display = 'none';
-        viewAccountsButton.style.display = 'none';
-        messageIndicator.style.display = 'none';
-    });
-});
